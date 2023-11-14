3e100cde4277d3ee04899f8779bd01ae1c414a7f
LENG-PC\LI <fuamrkk23@gmail.com>
2023-11-14 19:55:44 +0800

    home

--- src/hw7/Obj.java
+++ src/hw7/Obj.java
@@ -9,7 +9,9 @@ public class Obj {
 
 	public static void main(String[] args) throws IOException {
 		Dog dog = new Dog("Bob");
+		Dog dog1 = new Dog("Bob1");
 		Cat cat = new Cat("Sara");
+		Cat cat1 = new Cat("Sara1");
 
 		File dataF = new File("C:/javawork/data");
 		if (!dataF.exists()) {
@@ -22,9 +24,13 @@ public class Obj {
 		
 		oos.writeObject(dog);
 		System.out.println("dog寫入成功");
+		oos.writeObject(dog1);
+		System.out.println("dog1寫入成功");
 		
 		oos.writeObject(cat);
 		System.out.println("cat寫入成功");
+		oos.writeObject(cat1);
+		System.out.println("cat1寫入成功");
 
 		oos.close();
 		fos.close();
